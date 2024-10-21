616fd1fdfc3247c5776ae4787f6deb4159472363
abderrazekbhr <abderrazek455@gmail.com>
2024-10-21 18:30:34 +0200

    state.cpp

---
+++ src/shared/state/Game.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "state.h"
+#include "Game.h"
+using namespace std;
+using namespace state ;
+Game::Game(){
+    
+}
---
+++ src/shared/state/State.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "state.h"
+#include "State.h"
+using namespace std;
+using namespace state ;
+State::State(){
+    
+}
