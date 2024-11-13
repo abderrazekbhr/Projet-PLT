6a79d98e52f75fd3c654dcf545b89f37af5acb7e
abderrazekbhr <abderrazek455@gmail.com>
2024-11-13 08:48:19 +0100

    change test file

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -2,14 +2,14 @@
 #include <boost/test/unit_test.hpp>
 
 
-BOOST_AUTO_TEST_CASE(TestStaticAssert)
-{
-  BOOST_CHECK(1);
-}
+// BOOST_AUTO_TEST_CASE(TestStaticAssert)
+// {
+//   BOOST_CHECK(1);
+// }
 
-BOOST_AUTO_TEST_CASE(TestExemple)
-{
+// BOOST_AUTO_TEST_CASE(TestExemple)
+// {
   
-}
+// }
 
 /* vim: set sw=2 sts=2 et : */
