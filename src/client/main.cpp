be91e7f6891471f077298277890ae17ae2f968b3
KhawlaChedi <khawlachedi11@gmail.com>
2024-11-13 10:45:20 +0100

    update branch

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -22,12 +22,15 @@ int main(int argc,char* argv[])
     state::Game *game=new state::Game();
     game->setState(new state::InitState());
     game->request();
-    game->setState(new state::RoundState());
-    game->request();
-    game->setState(new state::DistributeCardState());
-    game->request();
-    game->setState(new state::CountScoreState());
-    game->request();
+    while (Game::players) {
+        game->setState(new state::RoundState());
+        game->request();
+        game->setState(new state::DistributeCardState());
+        game->request();
+        game->setState(new state::CountScoreState());
+        game->request();
+    }
+
 
 
     return 0;
