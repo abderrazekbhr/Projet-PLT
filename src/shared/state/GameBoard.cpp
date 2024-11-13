fd863f50fdbaefb13c9cbc6ef64cdf1ab264feb2
nour-benyoussef <nourbenyoussef73@gmail.com>
2024-11-13 11:01:03 +0100

    Update branch

---
+++ src/shared/state/GameBoard.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include "GameBoard.h"
+#include "Card.h"
+
+namespace state
+{
+
+    GameBoard::GameBoard()
+    {
+    }
+
+    GameBoard::~GameBoard()
+    {
+    }
+
+    int GameBoard::getNumberCardBoard()
+    {
+        return this->cardsOnBoard.size();
+    }
+
+    void GameBoard::addCardToBoard(Card card)
+    {
+        cardsOnBoard.push_back(card);
+    }
+
+    void GameBoard::deleteCard(Card card)
+    {
+        for (auto it = cardsOnBoard.begin(); it != cardsOnBoard.end(); it++)
+        {
+            if (*it == card)
+            {
+                cardsOnBoard.erase(it);
+            }
+        }
+    }
+
+    void GameBoard::displayCardsOnBoard()
+    {
+        std::cout << "Cards on Board : ";
+        for (Card &card : cardsOnBoard)
+        {
+            std::cout << "[" << card.getNumberCard() << "||" << card.getTypeCard() << "]";
+        }
+        std::cout << std::endl;
+    }
+
+};
