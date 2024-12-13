b1862e385c7072e58fb675ddcc6eb23ad83dc192
abderrazekbhr <abderrazek455@gmail.com>
2024-12-13 14:48:44 +0100

    change dia and add command getter and setter

--- test/shared/test_shared_throwCard.cpp
+++ test/shared/test_shared_throwCard.cpp
@@ -5,7 +5,6 @@
 #include "engine.h"
 
 using namespace state;
-using namespace engine;
 using namespace std;
 BOOST_AUTO_TEST_SUITE(ThrowCard)
 
@@ -13,7 +12,7 @@ BOOST_AUTO_TEST_SUITE(ThrowCard)
 BOOST_AUTO_TEST_CASE(all_throwCard_tests)
 {
     // Preparing new state
-    Engine *e = new Engine();
+    engine::Engine *e = new engine::Engine();
     e->init();
     State s1 = e->getState();
     int nbPlayer = 2;
@@ -26,11 +25,23 @@ BOOST_AUTO_TEST_CASE(all_throwCard_tests)
     {
         s1.addPlayer("User" + (i + 1));
     }
-    vector<Player*> players=s1.getAllPlayers();
-    NumberCard numbersCardOfCards[]={un,deux,trois};
-    NumberCard numbersCardOfCards[]={un,deux,trois};
+    vector<Player *> players = s1.getAllPlayers();
+    int size = 3;
+    NumberCard numbersOfCards[] = {un, deux};
+    TypeCard typesOfCards[] = {treffle, carreau};
+    for (auto p : players)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Card *c = new Card(numbersOfCards[i], typesOfCards[i]);
+            p->addHoldCard(*c);
+        }
+    }
 
-
-    
+    // Preparing Throw part
+    int indexCard = 1;
+    engine::ThrowCard *t = new engine::ThrowCard(indexCard);
+    // Check CMDTypeId
+    BOOST_CHECK_EQUAL(t->getCMDTypeId() , engine::THROW_CARD);
 }
 BOOST_AUTO_TEST_SUITE_END()
