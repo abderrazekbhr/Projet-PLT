65ceddd2a430149a84fcbccd601c4f448ccfd010
abderrazekbhr <abderrazek455@gmail.com>
2025-01-09 22:36:59 +0100

    fix bugs in testfile

--- test/shared/test_shared_RoundDistributeCard.cpp
+++ test/shared/test_shared_RoundDistributeCard.cpp
@@ -14,15 +14,15 @@ BOOST_AUTO_TEST_CASE(TestRoundDistributeCards_Execute)
     // Ajout de joueurs à l'état du jeu
     engine.getState().addPlayer("Player 1");
     engine.getState().addPlayer("Player 2");
-
+    engine.getState().initCards();
     // Vérification de l'état initial
     BOOST_CHECK_EQUAL(engine.getState().getAllPlayers().size(), 2); // Vérifie qu'il y a bien 2 joueurs
 
     // Vérification que le deck existe et contient des cartes
     auto cardsDeck = engine.getState().getAllCards();
-    BOOST_CHECK(cardsDeck);  // Vérifie que le deck existe (non nul)
+    BOOST_CHECK(cardsDeck);                            // Vérifie que le deck existe (non nul)
     size_t initialDeckSize = cardsDeck->getDeckSize(); // Stocke la taille initiale du deck
-    BOOST_CHECK_GT(initialDeckSize, 0); // Vérifie que le deck contient des cartes
+    BOOST_CHECK_GT(initialDeckSize, 0);                // Vérifie que le deck contient des cartes
 
     // Exécute la commande RoundDistributeCards
     BOOST_CHECK(roundDistribute.execute(&engine)); // Vérifie que l'exécution retourne true
